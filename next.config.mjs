c367a94b566bb89203419cc958e47f1241bdb502
maerzhase <markus@lili16.de>
2024-06-23 11:07:22 +0100

    fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@airgap/beacon-ui"],
+  },
+  webpack: (config) => {
+    // Used for connectkit to work with nextjs
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
